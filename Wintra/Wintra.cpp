1da1e822d83e215d3a48c166c854f5e3be8b63d9
Homuncoli <dan18765@spengergasse.at>
2020-10-25 20:40:27 +0100

    Updated Logtra

---
+++ Wintra/Wintra.cpp
@@ -0,0 +1,19 @@
+﻿// Wintra.cpp : Defines the entry point for the application.
+//
+
+#include "Wintra.h"
+
+#include "Logtra.h"
+
+using namespace std;
+
+int main()
+{
+	Logtra::Logger logger;
+
+	LGA_USE_LOGGER(logger);
+
+	LGA_SUCCESS("LGA Successfully started");
+
+	return 0;
+}
